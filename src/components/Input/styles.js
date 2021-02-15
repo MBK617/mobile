3838c62122a24c987e042a79e4d4e98c19cba22e
Abby Rictor <amrictor@gmail.com>
2021-02-14 20:44:32 -0700

    add reusable input component (authored by Isabella)

---
+++ src/components/Input/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  root: {
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+});
+
+export default styles;

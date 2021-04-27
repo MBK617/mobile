4ef5b80eecfc53e0792509110106b072b37fce46
Abby Rictor <amrictor@gmail.com>
2021-04-26 23:38:11 -0600

    move logged in screen to profile template (for now)

---
+++ src/containers/MorePage/components/ProfilePage/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  root: {
+    alignSelf: 'center',
+    flexShrink: 0,
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    width: '100%',
+    padding: 10,
+    minWidth: 250,
+  },
+  image: {
+    height: 200,
+    margin: 10
+  },
+});
+
+export default styles;

18ee2e843c2191ab4b6be73774ba47a61c0937b4
Abby Rictor <amrictor@gmail.com>
2021-05-18 22:37:13 -0600

    improve more page test coverage

--- src/containers/MorePage/tests/morePage.test.js
+++ src/containers/MorePage/tests/morePage.test.js
@@ -4,19 +4,23 @@ import { render, cleanup } from '@testing-library/react-native';
 import configureStore from 'redux-mock-store';
 import createSagaMiddleware from 'redux-saga';
 import { fromJS } from 'immutable';
+
 import useHistory from 'hooks/useHistory';
+import useLoginStatus from 'hooks/useLoginStatus';
 
 import MorePage from '..';
 
 jest.mock('hooks/useHistory');
-useHistory.mockImplementation(() => ({
-  history: ['home'],
-  path: 'home'
-}));
+
+jest.mock('hooks/useLoginStatus');
 
 const initialState = {
   app: fromJS({
-    token: '',
+    token: ''
+  }),
+  login:  fromJS({
+    errorMessage: '',
+    loading: false
   })
 };
 
@@ -30,16 +34,63 @@ describe('<MorePage />', () => {
     injectedSagas: {}, 
     runSaga: sagaMiddleware.run 
   });
+
+  describe('logged out navigation menu', () => {
+    beforeEach(() => {
+      store = mockStore(initialState);
+      useLoginStatus.mockImplementation(() => false);
+      useHistory.mockImplementation(() => ({
+        history: ['more', 'home'],
+        path: 'more'
+      }));    
+      rendered = render(<Provider store={store}><MorePage /></Provider>);
+    })
   
-  beforeEach(() => {
-    store = mockStore(initialState);
-    rendered = render(<Provider store={store}><MorePage /></Provider>);
-  })
-
-  afterEach(cleanup);
-
-  it('should render correctly', () => {
-    const tree = rendered.toJSON();
-    expect(tree).toMatchSnapshot();
+    afterEach(cleanup);
+  
+    it('should render without crashing', () => {
+      const tree = rendered.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
   });
+
+  describe('login page', () => {
+    beforeEach(() => {
+      store = mockStore(initialState);
+      useLoginStatus.mockImplementation(() => false);
+      useHistory.mockImplementation(() => ({
+        history: ['more/login', 'more', 'home'],
+        path: 'more/login'
+      }));    
+      rendered = render(<Provider store={store}><MorePage /></Provider>);
+    })
+  
+    afterEach(cleanup);
+  
+    it('should render without crashing', () => {
+      const tree = rendered.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  
+  describe('settings page', () => {
+    beforeEach(() => {
+      store = mockStore(initialState);
+      useLoginStatus.mockImplementation(() => true);
+      useHistory.mockImplementation(() => ({
+        history: ['more/settings', 'more', 'home'],
+        path: 'more/settings'
+      }));    
+      rendered = render(<Provider store={store}><MorePage /></Provider>);
+    })
+  
+    afterEach(cleanup);
+  
+    it('should render without crashing', () => {
+      const tree = rendered.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  
 });
+
